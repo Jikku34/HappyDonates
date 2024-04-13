7771b1739f0394f8dc410e3d4c9d1823e6ac00c4
Jikku James <jikkujames34@gmail.com>
2024-04-14 04:02:55 +0530

    2nd phase12

--- HappyDonates/settings.py
+++ HappyDonates/settings.py
@@ -116,7 +116,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-"""here aws"""
+"""AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+
+AWS_STORAGE_BUCKET_NAME = ''
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'"""
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
